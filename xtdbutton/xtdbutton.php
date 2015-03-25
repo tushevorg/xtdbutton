89223070965b8005d65359640caef2f997ca1609
tushevorg <git-site@tushev.org>
2015-03-25 21:39:29 +0500

    initial commit

---
+++ xtdbutton/xtdbutton.php
@@ -0,0 +1,36 @@
+<?php
+
+ // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport( 'joomla.plugin.plugin' );
+
+class plgButtonXtdbutton extends JPlugin {
+
+    function plgButtonXtdbutton(& $subject, $config)
+    {
+        parent::__construct($subject, $config);
+    }
+    function onDisplay($name)
+    {
+        $js =  "function sampleXTDButtonClick(editor) {
+					txt = prompt('Please enter ID','123');
+					if (!txt) return;
+					jInsertEditorText('{sampleplugin '+txt+'}', editor);
+				}";
+				
+        $doc = & JFactory::getDocument();
+        $doc->addScriptDeclaration($js);
+		
+        $button = new JObject;		
+		$button->modal = false;
+		$button->class = 'btn';
+		$button->link = '#';
+		$button->text = JText::_('Insert sampleplugin');
+		$button->name = 'wand';
+		$button->onclick = 'sampleXTDButtonClick(\''.$name.'\'); return false;';
+		
+        return $button;
+    }
+}
+?>
